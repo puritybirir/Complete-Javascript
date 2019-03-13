903236a152e02f9013376c9ac870655a5a9be55f
puritybirir <purity.birir@andela.com>
2019-03-13 21:43:32 +0300

    Variable mutations and type coercion

--- VariableMutation.js
+++ VariableMutation.js
@@ -2,4 +2,17 @@
  * Variable Mutation and type coercion
  */
 
- 
+ //Type coercion is the process of converting value from one type to another (such as string to number, object to boolean, and so on)
+
+ var firstName = "John";
+ var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = "married";
+isMarried = false;
+
+//Variable mutation
+age = 'twenty eight'
+job = "driver"
