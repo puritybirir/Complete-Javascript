f00839c66aa70efd9c043a87207d7d6efc27f0a7
puritybirir <purity.birir@andela.com>
2019-03-14 16:56:25 +0300

    Operators and operator precedence

---
+++ BasicOperators.js
@@ -0,0 +1,25 @@
+/******
+ * Basic Operators
+ */
+
+ var year, yearJohn, yearMark;
+ now = 2018;
+ ageJohn = 28;
+ ageMark = 33;
+
+ // Math operators
+ yearJohn = now - ageJohn;
+ yearMark = now - ageMark;
+
+ console.log(yearJohn);
+
+ console.log(now*2);
+
+ // Logical operators
+
+ var johnOlder = ageJohn < ageMark;
+ console.log(johnOlder)
+
+//typeof operator
+console.log(typeof(johnOlder))
+console.log(typeof(ageJohn))
---
+++ OperatorPrecedence.js
@@ -0,0 +1,24 @@
+/***
+ * Operator precedence
+ */
+
+ var now = 2018
+ var yearJohn = 1989
+ var fullAge = 18;
+
+ var isFullAge = now - yearJohn >= fullAge;
+ console.log(isFullAge);
+
+ var AgeJohn = now - yearJohn;
+ var ageMark = 35;
+ var average = AgeJohn + ageMark / 2;
+ console.log(average);
+
+ //Multiple assignments
+ var x, y;
+ x = y =(3 + 5)* 4 -6;
+ console.log(x, y);
+
+ // More operators
+ x *= 2;
+ console.log(x);
