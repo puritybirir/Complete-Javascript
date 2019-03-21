c5df338e833fe9cb706985a7a77b40e4da225c0d
puritybirir <purity.birir@andela.com>
2019-03-21 09:43:33 +0300

    If else statements

---
+++ boolean.js
@@ -0,0 +1,14 @@
+/**
+ * Boolean logic
+ */
+
+ var firstName = "Purity";
+ var age = 16;
+
+ if(age<13){
+     console.log(firstName + ' is a girl')
+ } else if(age>=13 && age<=19) {
+    console.log(firstName + ' is a teenager')
+ } else{ 
+    console.log(firstName + ' is a woman')
+ }
---
+++ ifElse.js
@@ -0,0 +1,15 @@
+/**
+ *  IF/Else statements
+ */
+
+ var firstName = 'John';
+ var civilStatus = 'single';
+
+ if (civilStatus === 'Married') {
+     console.log(firstName + " is Married")
+ } else {
+     console.log(firstName + " is not Married")
+ }
+
+ var isMarried = false;
+ 
