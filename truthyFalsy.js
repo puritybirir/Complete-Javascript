3b07ea540c563e5bd5f384d2edd5d87aa4f5182a
puritybirir <purity.birir@andela.com>
2019-03-21 10:14:42 +0300

    Understanding truthy and falsy values

---
+++ truthyFalsy.js
@@ -0,0 +1,21 @@
+/***
+ * Truth and Falsy values and equality operators
+ */
+
+ // falsy values: undefined, null, 0, '', NAN
+ // truthy values: NOT falsy values
+
+ var height;
+ height = 23;
+
+ if(height || height === 0){
+     console.log('Variable is defined');
+ } else {
+     console.log('Variable has NOT been defined')
+ }
+
+ // Equality Operators
+
+if(height == '23'){
+    console.log('The == operator does type coercion!')
+}
