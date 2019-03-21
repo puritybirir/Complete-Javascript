4d7b7fa2d63ee4a1056fdb8f5935f82a83907f43
puritybirir <purity.birir@andela.com>
2019-03-21 10:06:05 +0300

    Switch and Ternary operators

---
+++ switch.js
@@ -0,0 +1,17 @@
+// Switch statement
+
+var job = 'teacher';
+var firstName = 'Birir'
+switch (job) {
+    case 'teacher':
+    console.log(firstName + ' teaches kids how to code');
+    break;
+    case 'driver':
+    console.log(firstName + ' drives an uber in Lisbon.');
+    break;
+    case 'designer':
+    console.log(firstName + 'designs beautiful websites');
+    break;
+    default:
+    console.log(firstName + ' does something else')
+}
---
+++ ternary.js
@@ -0,0 +1,17 @@
+/******
+ * The Ternary operator and Switch statements
+ */
+var firstName = 'John';
+var age = 16;
+
+age >= 18? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice');
+
+var drink = age >= 18? 'beer' : 'juice';
+console.log(drink);
+
+// if(age >= 18){
+//     var drink ='beer'
+// } else{
+//     var drink ="juice"
+// }
+
